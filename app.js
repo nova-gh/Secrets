d1572bf75b2fac78e704c048228f907213ca92a7
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2020-12-27 10:24:23 -0800

    added get method for all view pages

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 let port = process.env.PORT || 3000;
 const app = express();
-app.set("view engine", "ejs");
-app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
-);
+//ejs- use ejs as view engine
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+//static files from pub folder
 app.use(express.static("public"));
-//TODO
+//get method
+app.get("/", (req, res) => {
+	res.render("home");
+});
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+app.get("/register", (req, res) => {
+	res.render("register");
+});
 app.listen(port, () => {
 	console.log(`Server is up! Local Port: ${port}!`);
 });
