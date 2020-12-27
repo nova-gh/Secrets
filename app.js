812c19d677330459156cf8181c7ecb9340a82e67
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2020-12-27 10:34:15 -0800

    db connection, schema

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 let port = process.env.PORT || 3000;
 const app = express();
 //ejs- use ejs as view engine
@@ -8,6 +9,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 //static files from pub folder
 app.use(express.static("public"));
+//==Mongoose Connection
+
+mongoose.connect(
+	"mongodb+srv://admin:admin413@cluster0.hgnmf.mongodb.net/secretsDB",
+	{ useNewUrlParser: true, useUnifiedTopology: true }
+);
+//==DB scehma
+const userSchema = new mongoose.Schema({
+	email: String,
+	password: String,
+});
 //get method
 app.get("/", (req, res) => {
 	res.render("home");
