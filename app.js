1eb0828f459613dafd7ec17123fec7da6f8d16d5
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2020-12-27 10:05:11 -0800

    Server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+let port = process.env.PORT || 3000;
+const app = express();
+app.set("view engine", "ejs");
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.static("public"));
+//TODO
+app.listen(port, () => {
+	console.log(`Server is up! Local Port: ${port}!`);
+});
