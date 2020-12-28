6157d79f4ed01361a3094e2f119c70c34983a2ef
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2020-12-27 22:36:59 -0800

    Required packages
    PACKAGES: passport passport-local passport-local-mongoose express-session

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 let port = process.env.PORT || 3000;
 const app = express();
 //ejs- use ejs as view engine
@@ -12,6 +14,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 //static files from pub folder
 app.use(express.static("public"));
+//express-session
+
 //==Mongoose Connection
 
 mongoose.connect(
@@ -36,45 +40,14 @@ app
 	.get((req, res) => {
 		res.render("register");
 	})
-	.post((req, res) => {
-		bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-			// Store hash in your password DB.
-			const newUser = new User({
-				email: req.body.username,
-				password: hash,
-				//hashed pass
-			});
-			newUser.save((err) => {
-				if (!err) {
-					res.render("secrets");
-				} else {
-					res.send(err);
-				}
-			});
-		});
-	});
+	.post((req, res) => {});
 //login page route
 app
 	.route("/login")
 	.get((req, res) => {
 		res.render("login");
 	})
-	.post((req, res) => {
-		const username = req.body.username;
-		const password = req.body.password;
-		//find query
-		User.findOne({ email: username }, (err, foundUser) => {
-			if (!err) {
-				if (foundUser) {
-					bcrypt.compare(password, foundUser.password, function (err, result) {
-						if (result === true) {
-							res.render("secrets");
-						}
-					});
-				}
-			}
-		});
-	});
+	.post((req, res) => {});
 
 app.listen(port, () => {
 	console.log(`Server is up! Local Port: ${port}!`);
