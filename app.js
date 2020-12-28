222ddf7ba92aaa2b664da061788d3abb7450443d
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2020-12-27 23:44:15 -0800

    login post route update

--- app.js
+++ app.js
@@ -90,7 +90,22 @@ app
 	.get((req, res) => {
 		res.render("login");
 	})
-	.post((req, res) => {});
+	.post((req, res) => {
+		const user = new User({
+			username: req.body.username,
+			password: req.body.password,
+		});
+		req.login(user, (err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				passport.authenticate("local")(req, res, () => {
+					//since user registered direct them to secrets
+					res.redirect("/secrets");
+				});
+			}
+		});
+	});
 
 app.listen(port, () => {
 	console.log(`Server is up! Local Port: ${port}!`);
